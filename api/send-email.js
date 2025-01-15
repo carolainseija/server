244b1dd2d919d1c85f4163bdbdbcbb735c9f7db0
carolainseija <carolainsilva1@gmail.com>
2025-01-15 13:24:28 -0300

    vercel

--- api/send-email.js
+++ api/send-email.js
@@ -1,11 +1,7 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
-
-// Cargar las variables de entorno desde el archivo .env
 dotenv.config();
-
 export default async function handler(req, res) {
-    // Configurar transporte SMTP
     const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
@@ -13,8 +9,6 @@ export default async function handler(req, res) {
             pass: process.env.EMAIL_PASS   // Acceder a la variable de entorno
         }
     });
-
-    // Opciones del correo
     const mailOptions = {
         from: process.env.EMAIL_USER,   // Usar variable de entorno
         to: req.body.to,
@@ -27,7 +21,6 @@ export default async function handler(req, res) {
             }
         ]
     };
-
     try {
         await transporter.sendMail(mailOptions);
         res.status(200).json({ message: "Correo enviado con éxito" });
