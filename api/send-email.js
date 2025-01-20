fc19b727ffbcfc8e52a95b59ea2469124b833ccd
carolainseija <carolainsilva1@gmail.com>
2025-01-20 20:24:02 -0300

    server ruta

--- api/send-email.js
+++ api/send-email.js
@@ -7,7 +7,7 @@ import ExcelJS from "exceljs";
 export default async function handler(req, res) {
   // Configurar los encabezados CORS
 
-  res.setHeader("Access-Control-Allow-Origin", "https://server-amber-nine-83.vercel.app/"); 
+  res.setHeader("Access-Control-Allow-Origin", "*"); 
   // https://server-amber-nine-83.vercel.app/api/send-email// Asegúrate de permitir la URL correcta de tu frontend
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
